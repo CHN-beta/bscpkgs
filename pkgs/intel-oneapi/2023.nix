bd7d5b02b59c4807e551a43f43489f79206e326a
chn <chn@chn.moe>
2025-05-28 19:57:10 +0800

    Revert "fix"
    This reverts commit d5708747b2b2489621565e7896730f4924dae476.

--- pkgs/intel-oneapi/2023.nix
+++ pkgs/intel-oneapi/2023.nix
@@ -12,11 +12,12 @@
 , autoPatchelfHook
 , symlinkJoin
 , libfabric
-, gcc13
+, gcc
+, gcc7
 , wrapCCWith
 , linuxHeaders
 , config
-}: let gcc = gcc13; in
+}:
 
 # The distribution of intel packages is a mess. We are doing the installation
 # based on the .deb metapackage "intel-hpckit", and follow de dependencies,
@@ -383,7 +384,7 @@ let
           rsync -a compiler/include/ $out/include/ # Intrinsics for icc
           rsync -a include/ $out/include/
           chmod +w $out/include
-          ln -s $out/lib/clang/17/include/ $out/include/icx # For icx
+          ln -s $out/lib/clang/16.0.0/include/ $out/include/icx # For icx
         popd
 
         # Manuals
@@ -391,9 +392,6 @@ let
       popd
 
       ln -s $out/lib $out/lib_lin
-
-      chmod +w $out/lib/x64
-      rm $out/lib/x64/libintelocl_emu.so
     '';
   };
 
